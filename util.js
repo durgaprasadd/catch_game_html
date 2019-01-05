cd9f1a111c05bc94d4bdc6e50b5200496624ede7
Durga Prasad <44020610+durgaprasadd@users.noreply.github.com>
2019-01-05 09:56:50 +0530

    Modified time taken.

--- util.js
+++ util.js
@@ -1,32 +1,34 @@
 let randomNumber = function(number) {
-    return Math.floor(Math.random() * number);
-  };
+  return Math.floor(Math.random() * number);
+};
 
-  let youWon = function(interval) {
-    alert('you won and time taken : ' + count);
-    count = 0;
-    clearInterval(interval);
-  };
+let youWon = function(timeInterval) {
+  let timeTaken = (count * timeInterval) / 1000;
+  alert('you won and time taken : ' + timeTaken + ' seconds');
+  count = 0;
+};
 
-  const randomColour = function() {
-    return `rgb(${randomNumber(255)},${randomNumber(255)},${randomNumber(
-      255
-    )})`;
-  };
-  const randomPixels = function(number) {
-    return `${randomNumber(number)}px`;
-  };
+const randomColour = function() {
+  return `rgb(${randomNumber(255)},${randomNumber(255)},${randomNumber(255)})`;
+};
+const randomPixels = function(number) {
+  return `${randomNumber(number)}px`;
+};
 
-  let changeColorAndPosition = function() {
-    document.getElementById('main').style.color = randomColour();
-    document.getElementById('main').style.background = randomColour();
-    document.getElementById('main').style.left = randomPixels(1230);
-    document.getElementById('main').style.top = randomPixels(670);
-    document.getElementById('main').onclick = youWon;
-    count++;
-  };
-  let count = 0;
+let changeColorAndPosition = function(timeInterval) {
+  document.getElementById('main').style.color = randomColour();
+  document.getElementById('main').style.background = randomColour();
+  document.getElementById('main').style.left = randomPixels(1230);
+  document.getElementById('main').style.top = randomPixels(670);
+  document.getElementById('main').onclick = youWon.bind(timeInterval);
+  count++;
+};
+let count = 0;
 
-  let randomPositionAndColour = function() {
-    let interval = setInterval(changeColorAndPosition, 500);
-  };
+let randomPositionAndColour = function() {
+  let timeInterval = 500;
+  let interval = setInterval(
+    changeColorAndPosition.bind(null, timeInterval),
+    timeInterval
+  );
+};

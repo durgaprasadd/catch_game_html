8d02a07c949f65bf1d52594c99091f8a505def2f
Durga Prasad <44020610+durgaprasadd@users.noreply.github.com>
2019-01-05 14:08:49 +0530

    Refactored clear interval.

--- util.js
+++ util.js
@@ -2,10 +2,14 @@ let randomNumber = function(number) {
   return Math.floor(Math.random() * number);
 };
 
-let youWon = function(timeGap) {
+let youWon = function(timeGap, interval) {
   let timeTaken = (count * timeGap) / 1000;
   let message = 'you won and time taken : ' + timeTaken + ' seconds';
   alert(message);
+  clearInterval(interval);
+  document.getElementById('main').onclick = randomPositionAndColour;
+  document.getElementById('main').style.top = '0px';
+  document.getElementById('main').style.left = '0px';
   count = 0;
 };
 
@@ -16,17 +20,23 @@ const randomPixels = function(number) {
   return `${randomNumber(number)}px`;
 };
 
-let changeColorAndPosition = function(timeGap) {
+let changeColorAndPosition = function(timeGap, interval) {
   document.getElementById('main').style.color = randomColour();
   document.getElementById('main').style.background = randomColour();
   document.getElementById('main').style.left = randomPixels(1230);
   document.getElementById('main').style.top = randomPixels(670);
-  document.getElementById('main').onclick = youWon.bind(null, timeGap);
+  document.getElementById('main').onclick = youWon.bind(
+    null,
+    timeGap,
+    interval
+  );
   count++;
 };
 let count = 0;
 
 let randomPositionAndColour = function() {
   let timeGap = 500;
-  setInterval(changeColorAndPosition.bind(null, timeGap), timeGap);
+  let interval = setInterval(function() {
+    changeColorAndPosition(timeGap, interval);
+  }, timeGap);
 };
